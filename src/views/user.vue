b470444f01ed440a4de973d51e57f0f86e630dd1
shlee9605 <shlee9605@naver.com>
2022-12-26 17:41:19 +0900

    ver2.0

--- src/views/user.vue
+++ src/views/user.vue
@@ -1,46 +1,53 @@
 <template>
   <div>
     <h1 style="margin-left: 2%">유저 조회</h1>
-    <div class="search-engine">
-      <input
-        class="form-control"
-        type="text"
-        name="아이디"
-        v-model="Search.userid"
-        autofocus
-        placeholder="아이디"
-      />
-      <input
-        class="form-control"
-        type="text"
-        name="이름"
-        v-model="Search.name"
-        autofocus
-        placeholder="이름"
-      />
-      <form style="margin-left: 2%;" type="submit" @submit.prevent="searchUserList">
-        <button class="btn-success" type="submit">Search</button>
+    <p class="loading" style="margin-left: 2%" v-if="loading">
+      검색 중입니다...
+    </p>
+    <div v-else>
+      <form
+        type="submit"
+        @submit.prevent="searchUserList"
+      >
+        <div class="search-engine">
+          <input
+            class="form-control"
+            type="text"
+            name="이름"
+            v-model="Search.name"
+            autofocus
+            placeholder="이름"
+          />
+          <input
+            class="form-control"
+            type="text"
+            name="아이디"
+            v-model="Search.userid"
+            autofocus
+            placeholder="아이디"
+          />
+          <button class="btn-success" style="margin-left:2%" type="submit">Search</button>
+        </div>
       </form>
-      <!-- <p class="loading" v-if="loading">검색 중입니다...</p> -->
+      <table class="table">
+        <tr style="background-color: rgba(0, 200, 0, 0.1);">
+          <td class="table-section-name">이름</td>
+          <td class="table-section-userid">아이디</td>
+          <td class="table-section-role">파</td>
+          <td class="table-section-age">나이</td>
+          <td class="table-section-email">이메일</td>
+          <td class="table-section-date">생성일자</td>
+        </tr>
+        <tr v-for="user in userList" :key="user.id">
+          <td class="table-section-name">{{ user.name }}</td>
+          <td class="table-section-userid">{{ user.userid }}</td>
+          <td class="table-section-role">{{ user.role }}</td>
+          <td class="table-section-age">{{ user.age }}</td>
+          <td class="table-section-email">{{ user.email }}</td>
+          <td class="table-section-date">{{ user.createdAt }}</td>
+        </tr>
+      </table>
     </div>
-    <table class="table">
-      <tr>
-        <td class="table-section">아이디</td>
-        <td class="table-section">이름</td>
-        <td class="table-section">파</td>
-        <td class="table-section">나이</td>
-        <td class="table-section">이메일</td>
-        <td class="table-section">생성일자</td>
-      </tr>
-      <tr v-for="user in userList" :key="user.id">
-        <td class="table-section">{{user.userid}}</td>
-        <td class="table-section">{{user.name}}</td>
-        <td class="table-section">{{user.role}}</td>
-        <td class="table-section">{{user.age}}</td>
-        <td class="table-section">{{user.email}}</td>
-        <td class="table-section">{{user.createdAt}}</td>
-      </tr>
-    </table>
   </div>
 </template>
 
@@ -55,7 +62,7 @@ export default {
         { key: "age", label: "나이" },
         { key: "role", label: "권한" },
         { key: "email", label: "이메일" },
-        { key: "createdAt", label: "생성일" }
+        { key: "createdAt", label: "생성일" },
       ],
       Search: {
         name: null,
@@ -65,17 +72,20 @@ export default {
   },
   computed: {
     userList() {
-      return this.$store.getters.UserList
+      return this.$store.getters.UserList;
+    },
+    loading() {
+      return this.$store.getters.SearchLoading;
     },
   },
   created() {
-    this.searchUserList()
+    this.searchUserList();
   },
   methods: {
     searchUserList() {
-      this.$store.dispatch('actUserList', this.Search)
+      this.$store.dispatch("actUserList", this.Search);
     },
-  }
+  },
 };
 </script>
 
