9d0b410a9ff0b6bd6affd990697ea9fd1bac0df0
shlee9605 <shlee9605@naver.com>
2022-12-26 19:55:57 +0900

    ver3.0

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,14 +1,20 @@
 <template>
   <div>
-    <h1 style="margin-left:2%">Home</h1>
-    <p style="margin-left:2%">
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam doloremque fugit voluptatum illo nemo nihil
-      pariatur perferendis ad atque similique officiis, sapiente accusantium eos odit aspernatur deserunt. Blanditiis,
-      aut molestias.
-    </p>
+    <h1 style="margin-left: 2%">Home</h1>
+    <div style="display:flex;">
+      <div class="main">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam
+        doloremque fugit voluptatum illo nemo nihil pariatur perferendis ad
+        atque similique officiis, sapiente accusantium eos odit aspernatur
+        deserunt. Blanditiis, aut molestias.
+      </div>
+      <div style="margin-left:5%">togle box</div>
+    </div>
   </div>
 </template>
 
 <script>
-export default {}
+export default {};
 </script>
+
+<style src="../assets/style/Home.css" scoped></style>
